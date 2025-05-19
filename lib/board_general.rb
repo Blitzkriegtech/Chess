9a0876000312a1e0f9e0b99ecc7640c7eb815ae3
Aljun D. Rendon <aljunrendon@yahoo.com>
2025-05-20 06:14:42 +0800

    add getter method

--- lib/board_general.rb
+++ lib/board_general.rb
@@ -5,4 +5,12 @@ module BoardGeneral
   def initialize
     @grid = Array.new(8) { Array.new(8) }
   end
+
+  def [](position)
+    row, col = position
+    # bound checking
+    return nil unless row.between?(0, 7) && col.between?(0, 7)
+
+    @grid[row][col]
+  end
 end
