28cf866ff3be1b15772be5f0f01c4c53b13c9a63
Aljun D. Rendon <aljunrendon@yahoo.com>
2025-05-20 07:52:29 +0800

    add move a chess piece caller method

--- lib/board_general.rb
+++ lib/board_general.rb
@@ -18,4 +18,20 @@ module BoardGeneral
     row, col = position
     @grid[row][col] = value
   end
+
+  def move_chess_piece!(move)
+    from, to = move
+    # bound checking
+    unless from.is_a?(Array) && from.length == 2 && from.all? { |coord| coord.between?(0, 7) } &&
+      to.is_a?(Array) && to.length == 2 && to.all? { |coord| coord.between?(0, 7) }
+      raise ArgumentError, "Invalid move format or out of bounds: #{move.inspect}"
+    end
+
+    piece = self[from]
+    # ensure there is a piece at the from pos before attempting to move
+    raise InvalidMoveError, "No piece at from position #{from}" if piece.nil?
+
+    self[to] = piece # move the piece
+    self[from] = nil # empty the from tile/square
+  end
 end
