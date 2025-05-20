34c6ef7db7221adc99dfff462bd65767e26f8a82
Aljun D. Rendon <aljunrendon@yahoo.com>
2025-05-20 09:18:06 +0800

    add parse method

---
+++ lib/chess_parser.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# parser class
+class ChessParser
+  def self.parse(input)
+    case input.strip
+    when /\A([a-h][1-8])-([a-h][1-8])\z/i # case-insensitivity added
+      { command: :move, from: algebraic_to_coords(Regexp.last_match(1)), to: algebraic_to_coords(Regexp.last_match(2)) }
+    when /\Asave\s+(\w+)\z/i # case-insensitivity added
+      { command: :save, filename: Regexp.last_match(1) }
+    when /\Aload\s+(\w+)\z/i # Added case-insensitivity
+      { command: :load, filename: Regexp.last_match(1) }
+    else
+      raise InvalidInputError, 'Invalid input format. Use "e2-e4", "save <name>", or "load <name>".'
+    end
+  rescue InvalidInputError # re-raise specific error
+    raise
+  rescue => e # just in case there are any parsing errors
+    puts "Error parsing input: #{e.message}"
+    raise InvalidInputError, 'An unexpected error occured during input parsing.'
+  end
+end
