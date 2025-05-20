19fa4a3ecf9407e05bdba8a9b5db28d9f37fe0c5
Aljun D. Rendon <aljunrendon@yahoo.com>
2025-05-20 10:08:48 +0800

    add new directory for chess pieces files

--- lib/piece.rb
+++ lib/piece.rb
@@ -17,7 +17,17 @@ class Piece
     @moved = true
   end
 
+  # abstract methods
+  # This method does *not* consider if the move leaves the king in check.
   def symbol
     raise NotImplementedError, "#{self.class} must implement the 'symbol' method."
+  end
+
+  def valid_moves(_board, _from)
+    raise NotImplementedError,"#{self.class} must implement the 'valid_moves' method."
+  end
+
+  def attacks?(_board, _from, _target)
+    raise NotImplementedError, "#{self.class} must implement the 'attacks?' method."
   end
 end
